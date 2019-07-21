24a84b94783b15f1d53fb0d174e0e63baf1fe2fe
Dorine Lemière <dorine.lemiere@epita.fr>
2019-07-21 15:01:11 +0200

    fix archi

--- promo/README.md
+++
@@ -1,3 +0,0 @@
-# example-app
-Hi
-it's an example app for the best project of the year
--- promo/application/djangoapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from . import models
-# Register your models here.
-
-admin.site.register(models.Article)
-admin.site.register(models.Vente)
-admin.site.register(models.Promotions)
