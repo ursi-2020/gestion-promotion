40d40b987b1497477fa586fc3774bea2214cad00
Dorine Lemière <dorine.lemiere@epita.fr>
2019-09-25 12:53:43 +0200

    models: customers & products db

--- application/djangoapp/models.py
+++ application/djangoapp/models.py
@@ -28,3 +28,18 @@ class Vente(models.Model):
 class Users(models.Model):
     email = models.CharField(max_length=200)
     password = models.CharField(max_length=200)
+
+class Customers(models.Model):
+    firstName = models.CharField(max_length=200)
+    lastName = models.CharField(max_length=200)
+    fidelityPoint = models.IntegerField(default=0)
+    payment = models.IntegerField(default=0)
+    account = models.CharField(max_length=10, default="")
+
+class Products(models.Model):
+    codeProduit = models.CharField(max_length=200)
+    familleProduit = models.CharField(max_length=200)
+    descriptionProduit = models.CharField(max_length=200)
+    quantiteMin = models.PositiveIntegerField()
+    packaging = models.PositiveIntegerField()
+    prix = models.PositiveIntegerField()
