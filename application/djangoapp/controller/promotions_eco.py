65c61450a69af5dbe1c0ef04e3b5db51a052f776
Dorine Lemière <dorine.lemiere@epita.fr>
2019-10-04 20:05:57 +0200

    Controller: PromotionsEco index

---
+++ application/djangoapp/controller/promotions_eco.py
@@ -0,0 +1,15 @@
+from application.djangoapp.models import *
+from django.core import serializers
+from django.http import HttpResponse
+from django.forms.models import modelform_factory
+import json
+
+
+from django.views.decorators.csrf import csrf_exempt
+
+
+# Promotions - Index
+# Display a listing of the resource.
+def index(request): 
+    data = serializers.serialize("json", PromotionsEco.objects.all())
+    return HttpResponse(json.dumps(data),content_type='application/json')
